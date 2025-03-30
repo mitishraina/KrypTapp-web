e2869b40d6cedb9f3c8f6aa3233db6f792c7feb6
Mitish Raina <rainamitish7@gmail.com>
2025-03-30 13:24:34 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/KrypTapp-web",
-  plugins: [react()]
+  plugins: [react()],
+  base: "/KrypTapp-web/"
 })
